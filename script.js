4cb1f19fbfc03d817a14a945105881db330c4075
Aditya16-ux <xj2v7pdnzd@privaterelay.appleid.com>
2026-02-01 16:09:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,44 @@ function removeTask(index) {
 }
 
 renderTasks();
+let timer;
+let timeLeft = 25 * 60; // 25 minutes
+let running = false;
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  document.getElementById("timerDisplay").textContent =
+    `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function startTimer() {
+  if (running) return;
+  running = true;
+
+  timer = setInterval(() => {
+    if (timeLeft > 0) {
+      timeLeft--;
+      updateTimerDisplay();
+    } else {
+      clearInterval(timer);
+      running = false;
+      alert("Successful people are not gifted; they just work hard, then succeed on purpose.");
+    }
+  }, 1000);
+}
+
+function pauseTimer() {
+  clearInterval(timer);
+  running = false;
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  running = false;
+  timeLeft = 25 * 60;
+  updateTimerDisplay();
+}
+
+updateTimerDisplay();
